a1baa25284697ecfe6a2a4c19f066e7ad3320c9a
eloan <eloan@192.168.100.109>
2020-06-17 09:39:34 -0300

    ArvoreAVL

---
+++ java-estrutura-de-dados-emf/src/com/emf/arvoreavl/Elemento.java
@@ -0,0 +1,22 @@
+package com.emf.arvoreavl;
+
+
+public class Elemento {
+	
+	private int valor;
+	
+	public Elemento(int valor) {
+		this.valor = valor;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+	
+}
+
